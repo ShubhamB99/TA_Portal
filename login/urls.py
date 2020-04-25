fc643d331924c902fbd22b60495db05ae1a8577c
Shubham Bhardwaj <31105326+ShubhamB99@users.noreply.github.com>
2020-04-25 11:30:35 +0530

    Beta version

--- login/urls.py
+++ login/urls.py
@@ -1,10 +1,13 @@
 """URLs for login."""
 from django.urls import path
-from login.views import LoginViewSet
+from login.views import LoginViewSet,login_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('',login_view,name = "home2"),
+    path('home',login_view,name= "home"),
     path('login', LoginViewSet.as_view({'get': 'login'})),
     path('pass-login', LoginViewSet.as_view({'get': 'pass_login'})),
     path('login/get-user', LoginViewSet.as_view({'get': 'get_user'})),
-    path('logout', LoginViewSet.as_view({'get': 'logout'})),
+    path('logout', LoginViewSet.as_view({'get': 'logout'}),name = "logout"),
 ]
